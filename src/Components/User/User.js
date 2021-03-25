bdb7a10c054ee58ec185c7985c5aada9f2bb96ab
Oways <owayslfkf@gmail.com>
2021-03-25 14:04:17 +0200

    add data table

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,80 +2,53 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './User.css';
 import ReactDOM from 'react-dom';
+import Delete from '../Delete/Delete';
 import 'antd/dist/antd.css';
-import { Table, Tag, Space } from 'antd';
+import { Table } from 'antd';
+import Add from '../Add/Add';
 
 const URL = 'https://dummyapi.io/data/api';
-const APP_ID = '6059efeea2454da8cd5cba7c';
+const APP_ID = '605c5e8a99206ffc189b27d7';
 
-// const columns = [
-//     {
-//       title: 'FirstName',
-//       dataIndex: 'firstName',
-//       key: 'firstName',
-//     },
-//     {
-//       title: 'Id',
-//       dataIndex: 'id',
-//       key: 'id',
-//     },
-//     {
-//         title: 'Email',
-//         dataIndex: 'email',
-//         key: 'email',
-//     },
-//     {
-//         title: 'LastName',
-//         dataIndex: 'lastName',
-//         key: 'lastName',
-//     }
-// ];
+
 
 function User(){
 
-    const [ users , setUsers ] = useState([]);
+    const [ state, setState ] = useState();
+    const [ loading, setLoading ] = useState(false);
+
 
     useEffect(() => {
-        axios.get(`${URL}/user`, { headers: { 'app-id': APP_ID} })
-        .then((res) => {
-            for(let i=0; i<res.data.limit; i++){
-                const FirstName = res.data.data[i].firstName;
-                const Email = res.data.data[i].email;
-                const Id = res.data.data[i].id;
-                const LastName = res.data.data[i].lastName;
-                const Pic = res.data.data[i].picture;
-                users.push({
-                    FirstName : FirstName,
-                    Email : Email,
-                    Id : Id,
-                    LastName : LastName,
-                    Picture : Pic 
-                })
-            }
-            console.log(users)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+        getData();
     },[]);
+
+    const getData = async () => {
+        await axios.get(`${URL}/user/0P6E1d4nr0L1ntW8cjGU`, { headers: { 'app-id': APP_ID} })
+        .then((res) => {
+            setLoading(true);
+            setState({
+                firstName : res.data.firstName,
+                gender : res.data.gender,
+                lastName : res.data.lastName,
+                phone : res.data.phone,
+                picture : res.data.picture
+            }
+            );
+        })
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false));
+        
+    };
 
     return(
         <div className="user-wrapper">
-            {users.map((user) => {
-                return(
-                    <div key={user.Id} className="user-cont">
-                        <h2>{user.FirstName}</h2>
-                        <p>{user.Id}</p>
-                        <p>{user.Email}</p>
-                        <img src={user.Picture} className="user-pic"/>
-                    </div>
-                );
-            })}
+            <h1>{state.firstName}{state.lastName}</h1>
+            <p>{state.gender}</p>
+            <p>{state.phone}</p>
+            <img className="user-pic" src={state.picture}/>
         </div>
     );
-    // ReactDOM.render(<Table columns={columns} dataSource={data} />, document.getElementById('user-wrapper'));
 }
-
 
 
 export default User;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,94 @@
-import React from 'react';
-import User from '../../Components/User/User';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './Home.css';
+import ReactDOM from 'react-dom';
+import Delete from '../../Components/Delete/Delete';
+import 'antd/dist/antd.css';
+import { Table } from 'antd';
+import Add from '../../Components/Add/Add';
+
+const URL = 'https://dummyapi.io/data/api';
+const APP_ID = '605c5e8a99206ffc189b27d7';
+
+
 
 function Home(){
+
+    const [ state, setState ] = useState();
+    const [ loading, setLoading ] = useState(false);
+
+
+    useEffect(() => {
+        getData();
+    },[]);
+
+    const getData = async () => {
+        await axios.get(`${URL}/user`, { headers: { 'app-id': APP_ID} })
+        .then((res) => {
+            setLoading(true);
+            setState(
+                res.data.data.map(row => ({
+                    firstName : row.firstName,
+                    email : row.email,
+                    lastName : row.lastName,
+                    id : row.id,
+                    action : row.id,
+                    key: row.id
+                }))
+            );
+        })
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false));
+        
+    };
+
+    const columns = [
+        {
+            title: 'FirstName',
+            dataIndex: 'firstName',
+            key: 'firstName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'LastName',
+            dataIndex: 'lastName',
+            key: 'lastName',
+        },
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            key: 'x',
+            render: (e) => <Delete/>,
+        },
+    ];
+
     return(
         <div className="home-wrapper">
-            Hello
+            <div className="add-btn-cont">
+                <Add/>
+            </div>
+            {loading ? ("Loading ..."): (
+                <Table 
+                columns={columns} 
+                dataSource={state}
+                pagination={{ pageSize:10 }}
+                bordered={true}
+                size={'middle'}
+                />
+            )}
+            
         </div>
     );
 }
+
 
 export default Home;
